c75bc23846220828d612f1f1d58f9c8923fab8f2
rahmatalmubarak <rahmatalmubarak35@gmail.com>
2025-07-30 09:27:09 +0700

    fix: update production database configuration and log current environment

--- models/index.js
+++ models/index.js
@@ -5,9 +5,10 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV;
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+console.log(process.env.NODE_ENV);
 
 let sequelize;
 if (config.use_env_variable) {
